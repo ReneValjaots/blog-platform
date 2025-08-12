69251126570025729c3c28f172dc168c0fd42f10
ReneValjaots <rene.valjaots@gmail.com>
2025-08-12 22:44:20 +0300

    Delete category endpoint implemented

--- backend/blog/src/main/java/com/renev/blog/services/CategoryService.java
+++ backend/blog/src/main/java/com/renev/blog/services/CategoryService.java
@@ -3,9 +3,11 @@ package com.renev.blog.services;
 import com.renev.blog.domain.entities.Category;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface CategoryService {
     List<Category> getAllCategories();
     Category createCategory(Category category);
+    void deleteCategory(UUID id);
 
 }
